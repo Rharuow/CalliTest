b86ddc0dc44fab8509fd71510bd3782b492fa204
John-Luke <lucas_ennabledmu2@hotmail.com>
2019-06-30 14:25:39 -0300

    Adicionando componente nav

--- components/nav.php
+++ components/nav.php
@@ -4,35 +4,57 @@
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    width: 100%;
+    max-width: 100%;
     background-color: #112233;
     border-radius: 0px;
     font-size: 1.8rem;
     padding: 0 1em;
+    user-select: none;
   }
+
   header .brand {
     text-decoration: none;
+    color: #fff;
   }
+
   .header-brand-logo {
     width: 8vw;
   }
+
   .navbar {
-    width: 12%;
+    width: 14%;
+    min-height: 50px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     margin: 0;
+    padding: 0;
+    font-size: 1.3rem;
   }
+
+  .navbar::before, .navbar::after {
+    display: none;
+  }
+
   .navbar a {
+    color: #fff;
     text-decoration: none;
+    transition: all 0.2s;
+    padding: 15px;
+    height: 100%;
+  }
+
+  .navbar a:hover {
+    background-color: rgba(250, 250, 250, 0.25);
   }
 </style>
 
 <header>
-  <a class="brand" href="#">Callitest</a>
+  <a class="brand" href="#">Callitest</a> <?php /* $brand-title */ ?>
 
-  <a href=""><img class="header-brand-logo" src="interface/ufrn-logo.png" /></a>
+  <a href="/"><img class="header-brand-logo" src="interface/ufrn-logo.png" /></a> <?php /* $brand-logo */ ?>
 
-  <nav class="navbar">
+  <nav class="navbar"> <?php /* $menu-items */ ?>
     <a href="login.php">Login</a>
     <a href="signup.php">Sign up</a>
   </nav>
--- index.php
+++ index.php
@@ -10,9 +10,10 @@
     <meta name="author" content="Harysson Soares">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="_css/style.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  
+    <link rel="stylesheet" href="_css/reset.css"/>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>   -->
     <script src="_scripts/script_btn.js"></script>
   </head>
   <body>
